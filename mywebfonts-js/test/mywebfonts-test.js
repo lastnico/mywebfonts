eb250ef905c066bf7b190a0178c0719f533d493c
nicolas.ternisien <nicolas.ternisien@4e5e80d6-c70f-11dd-b6f5-21ad4398c266>
2009-01-07 16:15:59 +0000

    Improve actual API Add unit testing

--- mywebfonts-js/test/mywebfonts-test.js
+++ mywebfonts-js/test/mywebfonts-test.js
@@ -5,17 +5,24 @@ new Test.Unit.Runner({
 	 	this.assertEqual('rough-typewriter', MyWebFonts.parseFontFamily(' "mwf:rough-typewriter" '));
     	this.assertEqual('rough-typewriter', MyWebFonts.parseFontFamily("monospace, 'Arial','mwf:rough-typewriter' "));
     	this.assertEqual('rough-typewriter', MyWebFonts.parseFontFamily("monospace, 'Arial','mwf:rough-typewriter' "));
+    	this.assertEqual('rough-typewriter', MyWebFonts.parseFontFamily(' "mwf:rough-typewriter" '));
 
-    	this.assertEqual("rough-typewriter", MyWebFonts.parseFontFamily(' mwf:rough-typewriter'));
+    	this.assertEqual(null, MyWebFonts.parseFontFamily(" monospace, 'Arial'"));
     	this.assertEqual(null, MyWebFonts.parseFontFamily(' "monospace, '));
 	},
+
+	testParseFontSize: function() {
+		this.assertEqual("20", MyWebFonts.parseFontFamily(' 20px'));
+		this.assertEqual("20", MyWebFonts.parseFontFamily('     20px	'));
+		this.assertEqual("20", MyWebFonts.parseFontFamily('     20px	'));
+	},
 	
-	testLoadFontDatas: function() {
+	testAddFoundElement: function() {
 		// Setup
 		var element = new Element("div", { id: "elementTest1", style: "font-family: 'mwf:rough-typewriter'" } ).update("Test 1");
 		$$("body").first().insert(element);
 		
-		MyWebFonts.loadFontDatas($("elementTest1"));
+		MyWebFonts.addFoundElement($("elementTest1"));
 
 		var scriptElement = $$("head script").last();
 		
@@ -26,13 +33,35 @@ new Test.Unit.Runner({
 		$("elementTest1").remove();
 		scriptElement.remove();
 	},
+	
+
+	testLoadFontDatas: function() {
+		// Setup
+		MyWebFonts.loadFontDatas("rough-typewriter", null, null, null);
+
+		var scriptElement = $$("head script").last();
+		
+		this.assertEqual("text/javascript", scriptElement.type);
+		this.assertMatch(/.*\/font\/rough-typewriter\/datas/, scriptElement.src);
+		
+		// Cleanup
+		scriptElement.remove();
+	},
 
 	testDescendants: function() {
-		
-		var textNodes = MyWebFonts.findTextNodes($("toto"));
+		var element = new FoundElement($("toto"), null, null, null, null);
+		var textNodes = element.findTextNodes();
 		showTextArray(textNodes);
 
 	},
+	
+	testCreateFontDatasUrl: function() {
+		this.assertEqual("/font/fontname/datas", MyWebFonts.createFontDatasUrl("fontname", null, null, null));
+		this.assertEqual("/font/fontname/45px/datas", MyWebFonts.createFontDatasUrl("fontname", null, "45", null));
+		this.assertEqual("/font/fontname2/v:regular/10px/datas", MyWebFonts.createFontDatasUrl("fontname2", "regular", "10", null));
+		this.assertEqual("/font/fontname2/v:regular/10px/c:009933/datas", MyWebFonts.createFontDatasUrl("fontname2", "regular", "10", "009933"));
+		
+	}
 
  });
 
@@ -41,12 +70,12 @@ new Test.Unit.Runner({
  */
 function showArray(array) {
 	for (var index = 0; index < array.length; ++index) {
-		$("debug").insert("array[" + index + "] = " + array[index] + "\n");
+		MyWebFonts.debug("array[" + index + "] = " + array[index] + "\n");
 	}
 }
 
 function showTextArray(array) {
 	for (var index = 0; index < array.length; ++index) {
-		$("debug").insert("array[" + index + "] = " + array[index].nodeValue + "\n");
+		MyWebFonts.debug("array[" + index + "] = " + array[index].nodeValue + "\n");
 	}
 }
