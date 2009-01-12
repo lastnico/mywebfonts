fba0f1174e5f1246768d789655398e994af3cecd
nicolas.ternisien <nicolas.ternisien@4e5e80d6-c70f-11dd-b6f5-21ad4398c266>
2009-01-12 09:09:07 +0000

    Add License Add license header in source code Clean code Release first version, which is not production ready, and not even dev ready ;-)

--- mywebfonts-js/src/mywebfonts.js
+++ mywebfonts-js/src/mywebfonts.js
@@ -1,20 +1,40 @@
 /*  
- * MyWebFonts JavaScript API, version 0.2
- * December 16th, 2008
- * (c) 2008-2009 Nicolas Ternisien <nicolas.ternisien@gmail.com>
+ * My Web Fonts JavaScript API, version 0.2-dev
+ * January 9th, 2009
+ * (c) 2009 Nicolas Ternisien <nicolas.ternisien@gmail.com>
  *
- * Prototype is freely distributable under the terms of an MIT-style license.
- * For details, see the MyWebFonts web site: http://mywebfonts.minipoulpe.org/
+ * My Web Fonts is freely distributable under the terms of an MIT-style license.
+ * For details, see the My Web Fonts web site: http://www.mywebfonts.org/
+ * 
+ * The MIT License
+ * Copyright (c) 2009 Nicolas Ternisien <nicolas.ternisien@gmail.com>, http://www.mywebfonts.org
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  * -------------------------------------------------------------------------- */
 
 var MyWebFonts = {
-	version: '0.2',
+	version: '0.2-dev',
 
 	// Configuration
 	options: {
 		// Test for external request
-		//externalSite: "http://mywebfonts.minipoulpe.org"
+		//externalSite: "http://www.mywebfonts.org"
 		externalSite: 		"",
 		debug:				true
 	},
